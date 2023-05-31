5769c9bd5eb420bb08ec112d2236d066624999c3
HaitongC <haitongchen116@gmail.com>
2023-05-30 22:27:36 -0700

    task page listed out, missing check and edit button

--- task.py
+++ task.py
@@ -8,13 +8,18 @@ import help
 LARGEFONT =("Verdana", 35)
 MEDIUMFONT =("Verdana", 25)
 
+DEFAULTLIST = ["Brush your teeth", "Wash your face", "Take medication", "Eat and hydrate"]
+CHECKCOUNT = 0
+LISTCOUNT = len(DEFAULTLIST)
+
+def addTasks():
+    global LISTCOUNT
+    LISTCOUNT += 1
+
 # tasks page window
 class tasksPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Here are today's tasks", font = LARGEFONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-  
         # putting the home and settings button
         homeBtn = ttk.Button(self, text="HOME",
                              command = lambda : controller.show_frame(home.homePage))
@@ -24,7 +29,21 @@ class tasksPage(tk.Frame):
                                 command = lambda : controller.show_frame(setting.settingsPage))
         settingBtn.grid(row = 0, column = 10, padx = 10, pady = 10)
 
+        # put header text
+        label = ttk.Label(self, text ="Here are today's tasks", font = LARGEFONT)
+        label.grid(row = 1, column = 5, padx = 10, pady = 10)
+
+        # lay out default checklist items
+        c = 2
+        for item in DEFAULTLIST:
+            label = ttk.Label(self, text = item, font = MEDIUMFONT)
+            label.grid(row = c, column = 5, padx = 10, pady = 10)
+            c += 1
+
         # putting help button to link to help page
         helpBtn = ttk.Button(self, text = "HELP",
                              command = lambda : controller.show_frame(help.helpPage))
-        helpBtn.grid(row = 9, column = 10, padx = 10, pady = 10)
+        helpBtn.grid(row = 10, column = 10, padx = 10, pady = 10)
+
+        addBtn = ttk.Button(self, text = "Add Task", command = addTasks())
+        addBtn.grid(row = 8, column = 5, padx = 10, pady = 10)
