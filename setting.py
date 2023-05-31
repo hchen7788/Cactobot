0cb6275317caf2591cb2ed7e37bf8ad1f4334c7c
HaitongC <haitongchen116@gmail.com>
2023-05-30 23:51:42 -0700

    settings page layout

--- setting.py
+++ setting.py
@@ -8,6 +8,18 @@ import help
 LARGEFONT =("Verdana", 35)
 MEDIUMFONT =("Verdana", 25)
 
+def changeLightColor():
+    # TODO
+    print("change light color here")
+
+def changeMusic():
+    # TODO
+    print("change music here")
+
+def changeVolumeLevel():
+    # TODO
+    print("change volume level here")
+
 # settings page window
 class settingsPage(tk.Frame):
      
@@ -15,7 +27,7 @@ class settingsPage(tk.Frame):
          
         tk.Frame.__init__(self, parent)
         label = ttk.Label(self, text ="Settings", font = LARGEFONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+        label.grid(row = 1, column = 5, padx = 10, pady = 10)
   
         # putting the home and settings button
         homeBtn = ttk.Button(self, text="HOME",
@@ -25,19 +37,19 @@ class settingsPage(tk.Frame):
         settingBtn = ttk.Button(self, text ="SETTINGS",
                                 command = lambda : controller.show_frame(settingsPage))
         settingBtn.grid(row = 0, column = 10, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        taskBtn = ttk.Button(self, text ="Tasks",
-                            command = lambda : controller.show_frame(task.tasksPage))
-     
-        # putting the button in its place by
-        # using grid
-        taskBtn.grid(row = 9, column = 5, padx = 10, pady = 10)
 
         # putting help button to link to help page
         helpBtn = ttk.Button(self, text = "HELP",
                              command = lambda : controller.show_frame(help.helpPage))
         helpBtn.grid(row = 9, column = 10, padx = 10, pady = 10)
+
+
+        # lay out 3 settings button
+        lightBtn = ttk.Button(self, text = "Light Color", command = changeLightColor)
+        musicBtn = ttk.Button(self, text = "Music", command = changeMusic)
+        volumeBtn = ttk.Button(self, text = "Volume Level", command = changeVolumeLevel)
+        lightBtn.grid(row = 3, column = 1, padx = 10, pady = 10)
+        musicBtn.grid(row = 5, column = 1, padx = 10, pady = 10)
+        volumeBtn.grid(row = 7, column = 1, padx = 10, pady = 10)
   
   
