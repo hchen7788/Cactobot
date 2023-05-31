2cace2b2105a1ab8319bd3cc31c204ea23c9b043
HaitongC <haitongchen116@gmail.com>
2023-05-30 22:12:49 -0700

    clean up home page, need to add image

--- home.py
+++ home.py
@@ -8,10 +8,6 @@ import help
 LARGEFONT =("Verdana", 35)
 MEDIUMFONT =("Verdana", 25)
 
-def addTasks():
-    label = ttk.Label(text="open the add tasks page")
-    label.pack()
-
 
 # first window frame startpage
 class homePage(tk.Frame):
@@ -28,24 +24,20 @@ class homePage(tk.Frame):
                                 command = lambda : controller.show_frame(setting.settingsPage))
         settingBtn.grid(row = 0, column = 10, padx = 10, pady = 10)
   
-        ## task button to link to task page
-        taskBtn = ttk.Button(self, text ="TASKS",
-                             command = lambda : controller.show_frame(task.tasksPage))
-        taskBtn.grid(row = 5, column = 5, padx = 10, pady = 10)
         
         # label of header
         label = ttk.Label(self, text ="Hi I'm Cactcobot, your personal reminder robot", font = LARGEFONT)
-        # putting the grid in its place by using grid
         label.grid(row = 1, column = 5, padx = 10, pady = 10)
-
-        # label of frame Layout 2
-        label = ttk.Label(self, text ="Let's take a look at today's tasks", font = MEDIUMFONT)
-        # putting the grid in its place by using
+        # label of main text
+        label = ttk.Label(self, text ="Let's take a look at today's tasks!", font = MEDIUMFONT)
         label.grid(row = 2, column = 5, padx = 10, pady = 10)
 
+        # TODO: add image
 
-        # addBtn = ttk.Button(self, text="Add Tasks", command=addTasks)
-        # addBtn.grid(row = 2, column = 4, padx = 10, pady = 10)
+        # task button to link to task page
+        taskBtn = ttk.Button(self, text ="TASKS",
+                             command = lambda : controller.show_frame(task.tasksPage))
+        taskBtn.grid(row = 5, column = 5, padx = 10, pady = 10)
 
         # putting help button to link to help page
         helpBtn = ttk.Button(self, text = "HELP",
